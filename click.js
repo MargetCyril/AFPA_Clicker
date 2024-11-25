0eead5ad0122ba0a3790b99cf29608641c1092b3
MargetCyril <cyrilmarget@gmail.com>
2024-11-25 23:12:11 +0100

    with added bug

--- click.js
+++ click.js
@@ -2,24 +2,36 @@ let code = 0
 let autocodeur = 0
 let maitrise = 0
 let maitrisecheck = 0
+let bugs = 0
+let heure = 0
 
 function add(nombre) {
     for (let i=0; i <nombre ; i++) {
         code++;
+        let s =Math.random(100);
+        console.log(s);
+        if (s<0.25) {bugs++}
     }
 }
 
-windows.setinterval(function() {
+function bug(nombre) {
+    for (let i=0; i<nombre; i++) {
+        bugs--;
+    }
+}
+
+window.setInterval(function() {
 
     add(autocodeur)
     maitriser()
     document.getElementById("clickcount").innerHTML = "code: " + code;
     document.getElementById("maitrise").innerHTML = "maitrise "+ maitrise;
-},1000)
+    document.getElementById("bugcount").innerHTML = "bug "+ bugs;
+},100)
 
 function addautocode() {
     autocodeur++
-    document.getElementById("autocodeur").innerHTML = "autocodeur" + autocodeur;
+    document.getElementById("autocodeur").innerHTML = "autocodeur " + autocodeur;
 
 }
 
